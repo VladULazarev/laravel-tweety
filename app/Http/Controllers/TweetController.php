57c6a5001580a44a53bfc81ff3429d66256fb81b
VladULazarev <getyourbestsite@gmail.com>
2023-01-27 18:13:40 +0300

    Edit file

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tweet;
+use DB;
 
 class TweetController extends Controller
 {
+    //use Followable;
+
     /**
      * Display a listing of the resource.
      *
@@ -38,7 +41,7 @@ class TweetController extends Controller
      *
      * @see public\build\assets\app.js -- 4. Store and show the new 'tweet'
      *
-     * @return \Illuminate\Http\RedirectResponse
+     * @return \Illuminate\View\View
      */
     public function store()
     {
@@ -59,8 +62,37 @@ class TweetController extends Controller
      *
      * @param int request('tweetId') from public\build\assets\app.js -- 3. Delete the 'tweet'
      */
-    public function destroy()
+    public function destroy(): void
     {
         Tweet::find(request('tweetId'))->delete();
     }
+
+    /**
+     * Get new 'tweets' for the current user using 'ajax'
+     *
+     * @see public\build\assets\app.js --- 5. Get new 'tweets' for current user using 'ajax'
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getNewTweetsForCurrentUser()
+    {
+        # Get 'ids' of users which the current user is following
+        $ids = DB::table('follows')
+        ->where('user_id', request('userId'))
+        ->select('following_user_id')
+        ->pluck('following_user_id')
+        ->toArray();
+
+        # Get new 'tweets'
+        $tweets = Tweet::where( 'id', '>', request('tweetLastId') )
+        ->whereIn('user_id', $ids)
+        ->latest()
+        ->get();
+
+        # If there are new 'tweets'
+        if ($tweets->count()) {
+            return view('tweets.new-tweet-ajax', [ 'tweets' => $tweets ]);
+        }
+    }
+
 }
