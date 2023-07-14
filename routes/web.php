803ecbc92b60fcf7cc53c95e353364b54209eda3
VladULazarev <68382253+VladULazarev@users.noreply.github.com>
2023-07-14 15:17:45 +0300

    Code refactoring

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/clear', function () {
     Artisan::call('config:cache');
     Artisan::call('view:clear');
     Artisan::call('route:clear');
-
+    
     return "Cache cleared";
 });
 
@@ -32,48 +32,28 @@ Route::get('/', function () { return view('welcome'); });
 
 Route::middleware('auth')->group(function () {
 
-
-    // ---------------------------------------------------------------- Tweets
-
-    Route::get('/home', [
-        TweetController::class, 'index' ] )->name('tweets.index');
-
-    Route::post('/more-tweets', [
-        TweetController::class, 'moreTweets' ] )->name('more-tweets');
-
-    Route::post('/tweets', [ TweetController::class, 'store' ] )->name('store-tweet');
-
-    Route::post('/delete-tweet', [ TweetController::class, 'destroy' ] );
-
-    Route::post('/new-tweets', [ TweetController::class, 'getNewTweetsForCurrentUser' ] );
-
-
-    // --------------------------------------------------------------- Explore
-
-    Route::get('/explore', [ ExploreController::class, 'index' ] )->name('explore');
-
-
-    // -------------------------------------------------------- User's Profile
-
-    Route::get('/profiles/{user:username}', [
-        UserProfileController::class, 'show' ] )->name('user-profile');
-
-    Route::post('/more-profiles-tweets/{user:username}', [
-        UserProfileController::class, 'showMoreProfileTweets' ] )->name('more-profile-tweets');
-
-
-    // --------------------------------------------------------------- Follows
-
-    Route::post('/profiles/{user:username}/follow', [
-        FollowsController::class, 'store' ] )->name('follow');
-
-
-    // ------------------------------------------------ Default Laravel Profile
-
+    // Tweets
+    Route::get('/home', [TweetController::class, 'index'])->name('tweets.index');
+    Route::post('/more-tweets', [TweetController::class, 'moreTweets'])->name('more-tweets');
+    Route::post('/tweets', [TweetController::class, 'store'])->name('store-tweet');
+    Route::post('/delete-tweet', [TweetController::class, 'destroy']);
+    Route::post('/new-tweets', [TweetController::class, 'getNewTweetsForCurrentUser']);
+    
+    // Explore
+    Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
+    
+    // User's Profile
+    Route::get('/profiles/{user:username}', [UserProfileController::class, 'show'])->name('user-profile');
+    Route::post('/more-profiles-tweets/{user:username}', [UserProfileController::class, 'showMoreProfileTweets'])
+        ->name('more-profile-tweets');
+    
+    // Follows
+    Route::post('/profiles/{user:username}/follow', [FollowsController::class, 'store'])->name('follow');
+    
+    // Default Laravel Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
 
 require __DIR__.'/auth.php';
